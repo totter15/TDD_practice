dd41fe5632369d0145a1c76deb067cf1f1e1eb62
천진아 <totter15@naver.com>
2022-11-07 16:37:37 +0900

    init

---
+++ sum.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+	return a + b;
+}
+
+function sumOf(numbers) {
+	// let result = 0;
+	// numbers.forEach((n) => {
+	// 	result += n;
+	// });
+	// return result;
+
+	return numbers.reduce((acc, current) => acc + current, 0);
+}
+
+exports.sum = sum;
+exports.sumOf = sumOf;
